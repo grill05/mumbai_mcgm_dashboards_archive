8119834dc0009665c14cd50191c195c25f5a71d2
Your Name <you@example.com>
2021-12-23 12:27:27 +0530

    auto

--- scraper.py
+++ scraper.py
@@ -2,13 +2,16 @@ import os,requests,json,datetime,csv
 
 if __name__=='__main__':
   proxy='socks4://203.115.123.165:9999'
-  max_tries=100
+  max_tries=100;tries=0
+
   
   date=datetime.datetime.now();date_str=date.strftime('%d_%m_%Y')
   # ~ os.system('curl -# "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "mumbai_dashboards/'+date_str+'.pdf"')
-  tries=0
-  while (not os.path.exists("mumbai_dashboards/"+date_str+'.pdf"')) and (tries<max_tries):    
-    cmd='curl  -k -x "'+proxy+'" "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "mumbai_dashboards/'+date_str+'.pdf"'
+  dashboard_fname="mumbai_dashboards/"+date_str+'.pdf"'
+  while (not os.path.exists(dashboard_fname)) and (tries<max_tries):    
+    cmd='curl  -k -x "'+proxy+'" "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "'+dashboard_fname+'.pdf"'
+    print(cmd)
     os.system(cmd)
+    os.system('ls -a mumbai_dashboards/')
     tries+=1
 
