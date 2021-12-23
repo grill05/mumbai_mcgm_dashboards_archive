68acb30b5c30b2e978420feb7752653184e55dfd
Your Name <you@example.com>
2021-12-23 12:28:21 +0530

    auto

--- scraper.py
+++ scraper.py
@@ -7,9 +7,9 @@ if __name__=='__main__':
   
   date=datetime.datetime.now();date_str=date.strftime('%d_%m_%Y')
   # ~ os.system('curl -# "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "mumbai_dashboards/'+date_str+'.pdf"')
-  dashboard_fname="mumbai_dashboards/"+date_str+'.pdf"'
+  dashboard_fname="mumbai_dashboards/"+date_str+'.pdf'
   while (not os.path.exists(dashboard_fname)) and (tries<max_tries):    
-    cmd='curl  -k -x "'+proxy+'" "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "'+dashboard_fname+'.pdf"'
+    cmd='curl  -k -x "'+proxy+'" "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "'+dashboard_fname+'"'
     print(cmd)
     os.system(cmd)
     os.system('ls -a mumbai_dashboards/')
