1c74eaab7bc744f3e83782f1292acec0ead3441f
Your Name <you@example.com>
2021-12-23 11:41:11 +0530

    auto

--- scraper.py
+++ scraper.py
@@ -2,5 +2,5 @@ import os,requests,json,datetime,csv
 
 if __name__=='__main__':
   date=datetime.datetime.now();date_str=date.strftime('%d_%m_%Y')
-  os.system('wget "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -O "mumbai_dashboards/'+date_str+'.pdf"')
+  os.system('curl -# "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -o "mumbai_dashboards/'+date_str+'.pdf"')
 
