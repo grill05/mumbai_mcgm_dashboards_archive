a2f46230824bad39dc5eadc65b13e0c9cf9919b8
Your Name <you@example.com>
2021-12-23 11:38:00 +0530

    auto

--- scraper.py
+++ scraper.py
@@ -2,6 +2,5 @@ import os,requests,json,datetime,csv
 
 if __name__=='__main__':
   date=datetime.datetime.now();date_str=date.strftime('%d_%m_%Y')
- 
   os.system('wget "https://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf" -O "mumbai_dashboards/'+date_str+'.pdf"')
 
